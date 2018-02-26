23f2334f96d59b8ad6d3492e9858d999992c195c
Jakkrit Junrat <j.jakkrit@appswin.co.th>
2018-02-26 17:09:32 +0700

    First Push

---
+++ PersonApi/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonApi.Controllers
+{
+    [Route("api/[controller]")]
+    //[ApiExplorerSettings(IgnoreApi = true)]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        /// <summary>
+        /// Retrieves all values
+        /// </summary>
+        /// <remarks>Awesomeness!</remarks>
+        /// <response code="200">There are the values</response>
+        /// <response code="400">No values found</response>
+        /// <response code="500">Cannot retrieve values for a moment.</response>
+        [HttpGet]
+        [ProducesResponseType(typeof(List<string>), 200)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        [ProducesResponseType(typeof(void), 500)]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
